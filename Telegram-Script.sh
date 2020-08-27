55500b32c739e28a9ee1fc2a9bf21257f85a468a
Faruk GÖLLER <muhammedfaruk@protonmail.com>
2020-08-27 14:31:11 +0300

    Update Telegram-Script.sh

--- Telegram-Script.sh
+++ Telegram-Script.sh
@@ -5,9 +5,24 @@ wget -O telegram.tar.xz "https://telegram.org/dl/desktop/linux"  --no-check-cert
 sudo tar -xf telegram.tar.xz
 ##Çıkan Dosyayı /opt Dizinine Taşıyalım/Move The Extracted File To The opt Folder
 sudo mv Telegram /opt
-##İndirilen Dosyayı Temizleyelim
+##İndirilen Dosyayı Temizleyelim/Clear The Downloaded File
 sudo rm -rf telegram.tar.xz
-##Komutların Tamamlandı/Commands Finished
-echo Komutlar Tamamlandı Telegram çalıştırılacak
-##Telegram çalıştırılabilir dosyasını açalım, bu sayede .desktop dosyası da ototmatik oluşacak
-/opt/Telegram/Telegram
+##Başlatıcı Dosyamızı Oluşturalım/Create  The Launcher
+touch ~/.local/share/applications/telegramdesktop.desktop
+###Başlatıcımızın İçeriğini Yazalım/Write The Launcher's Content
+echo "[Desktop Entry]
+Version=1.0
+Name=Telegram Desktop
+Comment=Official desktop version of Telegram messaging app
+TryExec=/opt/Telegram/Telegram
+Exec=/opt/Telegram/Telegram -- %u
+Icon=telegram
+Terminal=false
+StartupWMClass=TelegramDesktop
+Type=Application
+Categories=Chat;Network;InstantMessaging;Qt;
+MimeType=x-scheme-handler/tg;
+Keywords=tg;chat;im;messaging;messenger;sms;tdesktop;
+X-GNOME-UsesNotifications=true" > ~/.local/share/applications/telegramdesktop.desktop
+##Komutların Tamamlandı/Script Finished
+echo Komutlar Tamamlandı/Script Finished
